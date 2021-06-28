4b8bac1ce535bbfc024775afef29c50abb032990
Yifang Ma <yifang@nearprotocol.com>
2021-06-28 16:51:39 +0800

    fix

--- src/effects/index.js
+++ src/effects/index.js
@@ -1,155 +1,160 @@
 // sign up
-export async function signUp(username, email,password, d) {
+export async function signUp(username, email, password, d) {
   let myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append('Content-Type', 'application/json');
 
   let raw = JSON.stringify({
-  "username": username,
-  "email": email,
-  "password": password});
-  
+    username: username,
+    email: email,
+    password: password,
+  });
+
   let requestOptions = {
     method: 'POST',
     headers: myHeaders,
     body: raw,
-    redirect: 'follow'
+    redirect: 'follow',
   };
-  
-  fetch("http://35.236.73.215/user/signup", requestOptions)
-    .then(response => response.text())
-    .then(result => {
-      console.log(result)
-      localStorage.setItem('user', result)
+
+  fetch('http://35.236.73.215/user/signup', requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      localStorage.setItem('user', result);
     })
-    .catch(error => {
-      d('setError',{error})
-      console.log('error', error)});
+    .catch((error) => {
+      d('setError', { error });
+      console.log('error', error);
+    });
 }
 
 // create project
 export async function createProject(projectName, d) {
   let myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append('Content-Type', 'application/json');
 
-  let user = localStorage.getItem('user')
-  user = JSON.parse(user)
-  let token = user.token
+  let user = localStorage.getItem('user');
+  user = JSON.parse(user);
+  let token = user.token;
 
-  myHeaders.append("Authorization", "Bearer " + token);
-  if(!token) {
-    console.log('No User found signup or login')
-    d('setError',{error:'No User found signup or login'})
-    return
+  myHeaders.append('Authorization', 'Bearer ' + token);
+  if (!token) {
+    console.log('No User found signup or login');
+    d('setError', { error: 'No User found signup or login' });
+    return;
   }
-  
+
   let raw = JSON.stringify({
-    "projectName": projectName
+    projectName: projectName,
   });
-  
+
   let requestOptions = {
     method: 'POST',
     headers: myHeaders,
     body: raw,
-    redirect: 'follow'
+    redirect: 'follow',
   };
 
-  fetch("http://35.236.73.215/project", requestOptions)
-    .then(response => response.text())
-    .then(result => {
-      console.log(result)
-      result = JSON.parse(result)
-      d('setProjectId',{projectId:result.project_id})})
-    .catch(error => {
-      d('setError',{error})
-      console.log('error', error)});
+  fetch('http://35.236.73.215/project', requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      d('setProjectId', { projectId: result.project_id });
+    })
+    .catch((error) => {
+      d('setError', { error });
+      console.log('error', error);
+    });
 }
 
 // create query
-export async function createQuery(title,query,chartType,projectId, d){
+export async function createQuery(title, query, chartType, projectId, d) {
   let myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-    
-  let user = localStorage.getItem('user')
-  user = JSON.parse(user)
-  let token = user.token
+  myHeaders.append('Content-Type', 'application/json');
 
-  myHeaders.append("Authorization", "Bearer " + token);
-  if(!token) {
-    console.log('No User found signup or login')
-    d('setError',{error:'No User found signup or login'})
-    return
+  let user = localStorage.getItem('user');
+  user = JSON.parse(user);
+  let token = user.token;
+
+  myHeaders.append('Authorization', 'Bearer ' + token);
+  if (!token) {
+    console.log('No User found signup or login');
+    d('setError', { error: 'No User found signup or login' });
+    return;
   }
 
   let raw = JSON.stringify({
-    "title": title,
-    "query": query,
-    "chartType": chartType,
-    "projectId": projectId
+    title: title,
+    query: query,
+    chartType: chartType,
+    projectId: projectId,
   });
-  
+
   let requestOptions = {
     method: 'POST',
     headers: myHeaders,
     body: raw,
-    redirect: 'follow'
+    redirect: 'follow',
   };
-  
-  fetch("http://35.236.73.215/query", requestOptions)
-    .then(response => response.text())
-    .then(result => {
-      console.log(result)
-      result = JSON.parse(result)
-      d('setQueryId',{queryId:result.query_id})})
-    .catch(error => {
-      d('setError',{error})
-      console.log('error', error)});
+
+  fetch('http://35.236.73.215/query', requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      d('setQueryId', { queryId: result.query_id });
+    })
+    .catch((error) => {
+      d('setError', { error });
+      console.log('error', error);
+    });
 }
 
-//get query 
-export async function getQuery(queryId,d) {
+//get query
+export async function getQuery(queryId, d) {
   let myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  
+  myHeaders.append('Content-Type', 'application/json');
+
   let requestOptions = {
     method: 'GET',
-    redirect: 'follow'
+    redirect: 'follow',
   };
-  
-  fetch("http://35.236.73.215/query/"+queryId, requestOptions)
-    .then(response => response.text())
-    .then(result => {
-      console.log(result)
-      result = JSON.parse(result)
-      d('setTitle',{title:result.title})
-      d('setOutputQuery',{query:result.query})
-      d('setChartType',{chartType:result.chart_type})
+
+  fetch('http://35.236.73.215/query/' + queryId, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      d('setTitle', { title: result.title });
+      d('setOutputQuery', { query: result.query });
+      d('setChartType', { chartType: result.chart_type });
     })
-    .catch(error => {
-      d('setError',{error})
-      console.log('error', error)});
+    .catch((error) => {
+      d('setError', { error });
+      console.log('error', error);
+    });
 }
 
-// get result 
+// get result
 export async function getResult(query, d) {
   let myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  console.log('come here')
-  let raw = JSON.stringify({"query": query});
-  
+  myHeaders.append('Content-Type', 'application/json');
+  console.log('come here');
+  let raw = JSON.stringify({ query: query });
+
   let requestOptions = {
     method: 'POST',
     headers: myHeaders,
     body: raw,
-    redirect: 'follow'
+    redirect: 'follow',
   };
-  
-  fetch("http://35.236.73.215/connect", requestOptions)
-    .then(response => response.text())
-    .then(result => {
-      console.log(result)
-      result = JSON.parse(result)
-      d('setResult',{result})})
-    .catch(error => {
-      d('setError',{error})
-      console.log('error', error)});
+
+  fetch('http://35.236.73.215/connect', requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      d('setResult', { result });
+    })
+    .catch((error) => {
+      d('setError', { error });
+      console.log('error', error);
+    });
 }
